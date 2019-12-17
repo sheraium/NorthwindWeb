872926f21beeb8d167d457279c7a72eb916b7976
peter <sheraium@gmail.com>
2019-12-17 23:26:41 +0800

    refactoring CustomersServiceTests

--- NorthwindWeb.UnitTests/CustomersServiceTests.cs
+++ NorthwindWeb.UnitTests/CustomersServiceTests.cs
@@ -1,32 +1,50 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NorthwindWeb.Models;
 using NorthwindWeb.Repository;
 using NorthwindWeb.Services;
 using NSubstitute;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace NorthwindWeb.UnitTests
 {
     [TestClass]
     public class CustomersServiceTests
     {
-        [TestMethod]
-        public void get_all()
+        private CustomersService _customersService;
+        private IRepository<Customer> _repository;
+        private IUnitOfWork _unitOfWork;
+        private int CustomersCount;
+
+        [TestInitialize]
+        public void SetUp()
         {
-            var unitOfWork = Substitute.For<IUnitOfWork>();
-            var repository = Substitute.For<IRepository<Customer>>();
-            var customersService = new CustomersService(unitOfWork, repository);
-            repository.LookupAll().Returns(new List<Customer>
+            _unitOfWork = Substitute.For<IUnitOfWork>();
+            _repository = Substitute.For<IRepository<Customer>>();
+            _customersService = new CustomersService(_unitOfWork, _repository);
+
+            var customers = new List<Customer>
             {
                 new Customer() {CustomerID = "ALFKI", ContactName = "Maria Anders"},
                 new Customer() {CustomerID = "ANATR", ContactName = "Ana Trujillo"},
                 new Customer() {CustomerID = "ANTON", ContactName = "Antonio Moreno"},
-            }.AsQueryable());
+            };
+            CustomersCount = customers.Count;
 
-            var customers = customersService.GetAll();
+            _repository.LookupAll().Returns(customers.AsQueryable());
+        }
 
-            Assert.AreEqual(3, customers.Count);
+        [TestMethod]
+        public void get_all()
+        {
+            CustomersCountShouldBe(CustomersCount);
+        }
+
+        private void CustomersCountShouldBe(int expected)
+        {
+            var customers = _customersService.GetAll();
+
+            Assert.AreEqual(expected, customers.Count);
         }
     }
 }
