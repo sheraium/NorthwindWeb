4c9bd75110055cc03ad8d070240dcc6384811140
peter <sheraium@gmail.com>
2019-12-17 23:20:12 +0800

    get all pass

---
+++ NorthwindWeb.UnitTests/CustomersServiceTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NorthwindWeb.Models;
+using NorthwindWeb.Repository;
+using NorthwindWeb.Services;
+using NSubstitute;
+
+namespace NorthwindWeb.UnitTests
+{
+    [TestClass]
+    public class CustomersServiceTests
+    {
+        [TestMethod]
+        public void get_all()
+        {
+            var unitOfWork = Substitute.For<IUnitOfWork>();
+            var repository = Substitute.For<IRepository<Customer>>();
+            var customersService = new CustomersService(unitOfWork, repository);
+            repository.LookupAll().Returns(new List<Customer>
+            {
+                new Customer() {CustomerID = "ALFKI", ContactName = "Maria Anders"},
+                new Customer() {CustomerID = "ANATR", ContactName = "Ana Trujillo"},
+                new Customer() {CustomerID = "ANTON", ContactName = "Antonio Moreno"},
+            }.AsQueryable());
+
+            var customers = customersService.GetAll();
+
+            Assert.AreEqual(3, customers.Count);
+        }
+    }
+}
