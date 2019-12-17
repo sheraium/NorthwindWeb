2a1c98795f8de6727adad666a5ed1f69f9599912
peter <sheraium@gmail.com>
2019-12-17 23:46:43 +0800

    refactoring CustomersServiceTests

--- NorthwindWeb.UnitTests/CustomersServiceTests.cs
+++ NorthwindWeb.UnitTests/CustomersServiceTests.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NorthwindWeb.Models;
 using NorthwindWeb.Repository;
 using NorthwindWeb.Services;
 using NSubstitute;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
 
 namespace NorthwindWeb.UnitTests
 {
@@ -24,7 +24,37 @@ namespace NorthwindWeb.UnitTests
             _unitOfWork = Substitute.For<IUnitOfWork>();
             _repository = Substitute.For<IRepository<Customer>>();
             _customersService = new CustomersService(_unitOfWork, _repository);
+        }
 
+        [TestMethod]
+        public void get_all()
+        {
+            GivenAllCustomers();
+            CustomersCountShouldBe(CustomersCount);
+        }
+
+        [TestMethod]
+        public void get_by_id()
+        {
+            GivenOneCustomer(new Customer() {CustomerID = "ANATR", ContactName = "Ana Trujillo"});
+            CustomerIdShould("ANATR");
+        }
+
+        private void CustomerIdShould(string customerId)
+        {
+            var customer = _customersService.GetById(customerId);
+
+            Assert.AreEqual(customerId, customer.CustomerID);
+        }
+
+        private void GivenOneCustomer(Customer customer)
+        {
+            _repository.GetSingle(Arg.Any<Expression<Func<Customer, bool>>>())
+                .Returns(customer);
+        }
+
+        private void GivenAllCustomers()
+        {
             var customers = new List<Customer>
             {
                 new Customer() {CustomerID = "ALFKI", ContactName = "Maria Anders"},
@@ -34,23 +64,6 @@ namespace NorthwindWeb.UnitTests
             CustomersCount = customers.Count;
 
             _repository.LookupAll().Returns(customers.AsQueryable());
-        }
-
-        [TestMethod]
-        public void get_all()
-        {
-            CustomersCountShouldBe(CustomersCount);
-        }
-
-        [TestMethod]
-        public void get_by_id()
-        {
-            _repository.GetSingle(Arg.Any<Expression<Func<Customer, bool>>>())
-                .Returns(new Customer() {CustomerID = "ANATR", ContactName = "Ana Trujillo"});
-
-            var customer = _customersService.GetById("ANATR");
-
-            Assert.AreEqual("Ana Trujillo", customer.ContactName);
         }
 
         private void CustomersCountShouldBe(int expected)
